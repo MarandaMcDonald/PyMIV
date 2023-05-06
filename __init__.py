85065e39043fd04e835b36c6f730c501e355a3d8
Maranda McDonald <mmcdon47@jh.edu>
2023-05-06 12:06:32 -0400

    updated init

--- __init__.py
+++ __init__.py
@@ -37,9 +37,6 @@ form = loadUi(uifile, dialog)
 def make_dialog():
     # entry point to PyMOL's API
     from pymol import cmd
-
-    # pymol.Qt provides the PyQt5 interface, but may support PyQt4
-    # and/or PySide as well
     from pymol.Qt import QtWidgets
     from pymol.Qt.utils import loadUi
     from pymol.Qt.utils import getSaveFileNameWithExt
@@ -50,14 +47,13 @@ def make_dialog():
     # populate the Window from our *.ui file which was created with the Qt Designer
     uifile = os.path.join(os.path.dirname(__file__), 'pymolGUI.ui')
     form = loadUi(uifile, dialog)
-
-    
     return dialog
 
 def run():
-    # get the data
+    # retreive PDB file data
     pdb_file = form.lineEdit.value()
 
     print('User Entered Filename', pdb_file)
 
-    form.disulfideFinder.connect(run)
+#test the application
+form.disulfideFinder.connect(run)
