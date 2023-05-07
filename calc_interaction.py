36ac1fce6d750152f63906e626c51ac4667e605a
Maranda McDonald <mmcdon47@jh.edu>
2023-05-07 14:36:55 -0400

    fixed bondfile.write

--- calc_interaction.py
+++ calc_interaction.py
@@ -965,9 +965,9 @@ def end_to_end_dist(filename=str):
             # pylint: disable=consider-using-f-string
             #pylint: disable=line-too-long
             print("Distance between Cα atoms of first and last residue: {:.2f} Å".format((calcdistance(extractxyz(first_line), extractxyz(last_line)))))
-            bondfile.write("dist end_to_end, /{}//{}/{}`{}/{},/{}//{}/{}`{}/{}\n".format(only_pdb_first(only_pdb_last(filename)), first_line[21:22],first_line[19:20],remove(first_line[23:26]),first_line[13:16], only_pdb_first(only_pdb_last(filename)),last_line[21:22],last_line[19:20],last_line[23:26],last_line[13:15]))
-            bondfile.write("show sticks, /{}//{}/{}`{}/{}\n".format(only_pdb_first(only_pdb_last(filename)), first_line[21:22],first_line[19:20],remove(first_line[23:26]),first_line[13:16]))
-            bondfile.write("show sticks, /{}//{}/{}`{}/{}\n".format(only_pdb_first(only_pdb_last(filename)),last_line[21:22],last_line[19:20],last_line[23:26],last_line[13:15]))
+            bondfile.write("dist end_to_end, /{}//{}/{}`{}/{},/{}//{}/{}`{}/{}\n".format(only_pdb_first(only_pdb_last(filename)), first_line[21:22],first_line[17:20],remove(first_line[23:26]),first_line[13:16], only_pdb_first(only_pdb_last(filename)),last_line[21:22],last_line[17:20],last_line[23:26],last_line[13:15]))
+            bondfile.write("show sticks, /{}//{}/{}`{}\n".format(only_pdb_first(only_pdb_last(filename)), first_line[21:22],first_line[17:20],remove(first_line[23:26])))
+            bondfile.write("show sticks, /{}//{}/{}`{}\n".format(only_pdb_first(only_pdb_last(filename)),last_line[21:22],last_line[17:20],last_line[23:26]))
             bondfile.write("set dash_color, yellow, end_to_end\n")
             #bondfile.write("hide labels, Non_WC_hbond")
             #bondfile.write("color atomic, (not elem C), /{}//{}/{}`{}/{}\n".format(only_pdb_first(only_pdb_last(filename)), first_line[21:22],first_line[19:20],remove(first_line[23:26]),first_line[13:16]))
