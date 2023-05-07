6096533553546853bea61c3561307c7384261a98
Maranda McDonald <mmcdon47@jh.edu>
2023-05-07 19:44:17 -0400

    Added render.ui

--- __init__.py
+++ __init__.py
@@ -1418,15 +1418,78 @@ def make_dialog():
         '''
         Run the actions in the end to end distance button
         '''
-        # retreive PDB file data
-        pdb_file = form.lineEdit.text()
-        # Error Code
-        if pdb_file == "":
-            print("Please input a valid .pdb file name")
-        else:
-            print('User Entered Filename:', pdb_file)
-            end_to_end_dist(pdb_file)
-            cmd.run("end_to_end.pml")
+        # global reference to avoid garbage collection of our dialog_render
+        dialog_render = None
+
+
+        def run_plugin_gui():
+            '''
+            Open our custom dialog_render
+            '''
+            global dialog_render
+
+            if dialog_render is None:
+                dialog_render = make_dialog()
+
+            dialog_render.show()
+
+
+        def make_dialog():
+            # entry point to PyMOL's API
+            from pymol import cmd
+
+            # pymol.Qt provides the PyQt5 interface, but may support PyQt4
+            # and/or PySide as well
+            from pymol.Qt import QtWidgets
+            from pymol.Qt.utils import loadUi
+            from pymol.Qt.utils import getSaveFileNameWithExt
+
+            # create a new Window
+            dialog_render = QtWidgets.Qdialog()
+
+            # populate the Window from our *.ui file which was created with the Qt Designer
+            uifile = os.path.join(os.path.dirname(__file__), 'render.ui')
+            form = loadUi(uifile, dialog_render)
+
+            # callback for the "Ray" button
+            def run():
+                # get form data
+                height = form.input_height.value()
+                width = form.input_width.value()
+                dpi = form.input_dpi.value()
+                filename = form.input_filename.text()
+                units = form.input_units.currentText()
+
+                # calculate dots per centimeter or inch
+                if units == 'cm':
+                    dots_per_unit = dpi * 2.54
+                else:
+                    dots_per_unit = dpi
+
+                # convert image size to pixels
+                width *= dots_per_unit
+                height *= dots_per_unit
+
+                # render the image
+                if filename:
+                    cmd.png(filename, width, height, dpi=dpi, ray=1, quiet=0)
+                else:
+                    cmd.ray(width, height, quiet=0)
+                    print('No filename selected, only rendering on display')
+
+            # callback for the "Browse" button
+            def browse_filename():
+                filename = getSaveFileNameWithExt(
+                    dialog_render, 'Save As...', filter='PNG File (*.png)')
+                if filename:
+                    form.input_filename.setText(filename)
+
+            # hook up button callbacks
+            form.button_ray.clicked.connect(run)
+            form.button_browse.clicked.connect(browse_filename)
+            form.button_close.clicked.connect(dialog_render.close)
+
+            return dialog_render
 
     # To connect clicking buttons to a value, text or command
     form.browse.clicked.connect(browse_filename)
